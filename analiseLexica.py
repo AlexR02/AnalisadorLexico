724be71cde9c4adfade9422737cfd66c370e6858
AlexR02 <alex021020@gmail.com>
2022-05-04 08:40:26 -0300

    Melhorando a analise léxica

--- analiseLexica.py
+++ analiseLexica.py
@@ -3,15 +3,16 @@ import re
 def analiseLexica():
     f = open("calculadoraBasica.c", "r") #Abre o arquivo para leitura
     lines = f.readlines()
-    lines.remove("\n")
 
     tokens = {}
+    qtdLines = 1
 
     for line in lines:
-        line = re.sub("[ ]{2,}", " ", line)
         line = re.sub("{", " {", line)
         line = re.sub(";", " ;", line)
         line = re.sub(",", " ,", line)
+        line = re.sub("=", " = ", line)
+        line = re.sub("[ ]{2,}", " ", line)
         if line.strip() != '':
             line = line.strip()
 
@@ -20,14 +21,18 @@ def analiseLexica():
                 tokens[line[0]] = "Palavra reservada"
                 biblioteca = re.sub("(<|>)","", line[1])
                 tokens[biblioteca] = "Biblioteca"
-            elif re.search("^int main\\(\\) {$", line):
+            elif re.search("^int main\\(\\)( {|)$", line):
                 line = line.split()
                 tokens[line[0]] = "Palavra reservada"
                 tokens[line[1]] = "Palavra reservada"
-                tokens[line[2]] = "Delimitador de início"
+                if len(line) == 3:
+                    tokens[line[2]] = "Delimitador de início"
+            elif line == "{":
+                tokens[line] = "Delimitador de início"
             elif line == "}":
                 tokens[line] = "Delimitador de fim"
             else:
+                aux = line
                 line = line.split()
                 for i in line:
                     if i == "int" or i == "return":
@@ -44,9 +49,17 @@ def analiseLexica():
                         tokens[i] = "Nome de variável"
                     elif re.search("^[0-9]{1,}$", i):
                         tokens[i] = "Constante numérica"
-
+                    elif i == ",":
+                        tokens[i] = "Separador"
+                    else:
+                        if i == "#include":
+                            i = aux
+                        print("O token '" + i + "' na linha " + str(qtdLines) + " é inválido")
+                        return
+        qtdLines += 1 
     for key in tokens:
         print(key + " - " + tokens[key])
+    
 
 def main():
     analiseLexica()
